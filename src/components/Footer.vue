8c412b6a549ea94488db501d803b0d44d7971aa7
ilhamsabir <ilhamsabir3@gmail.com>
2021-07-09 13:45:45 +0700

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="footer">
+		<div class="row justify-content-between align-items-center">
+			<div class="col">
+				<p class="font-size-sm mb-0">© Front. <span class="d-none d-sm-inline-block">2020 Htmlstream.</span></p>
+			</div>
+			<div class="col-auto">
+				<div class="d-flex justify-content-end">
+				<ul class="list-inline list-separator">
+					<li class="list-inline-item">
+					<a class="list-separator-link" href="#">FAQ</a>
+					</li>
+
+					<li class="list-inline-item">
+					<a class="list-separator-link" href="#">License</a>
+					</li>
+
+					<li class="list-inline-item">
+					<div class="hs-unfold">
+						<a class="js-hs-unfold-invoker btn btn-icon btn-ghost-secondary rounded-circle">
+							<i class="tio-command-key"></i>
+						</a>
+					</div>
+					</li>
+				</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<header class="navbar navbar-expand-xl navbar-fixed navbar-height navbar-flush navbar-container navbar-bordered default">
+			<div class="container">
+				<div class=" js-mega-menu navbar-nav-wrap hs-menu-initialized hs-menu-horizontal">
+					<NavbarBrand />
+					<NavbarSideMenu />
+					<NavbarMainMenu />
+				</div>
+			</div>
+		</header>
+	</div>
+</template>
+
+<script>
+import NavbarBrand from '@/components/parts/navbar/Brand.vue'
+import NavbarSideMenu from '@/components/parts/navbar/SideMenu.vue'
+import NavbarMainMenu from '@/components/parts/navbar/MainMenu.vue'
+export default {
+	components: {
+		NavbarBrand,
+		NavbarSideMenu,
+		NavbarMainMenu
+	}
+}
+</script>
+
+<style>
+
+</style>
